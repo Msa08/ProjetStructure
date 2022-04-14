1d8d1bba7647bdcd2243c06f9ea3544ed1172ef9
KacemMIPI <3804052@ppti-24-304-04.ufr-info-p6.jussieu.fr>
2022-04-14 12:33:38 +0200

    ajout block.h

---
+++ block.h
@@ -0,0 +1,23 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+#include "hachage.h"
+
+
+typedef struct block {
+	Key * author ;
+	CellProtected * votes ;
+	unsigned char * hash ;
+	unsigned char * previous_hash ;
+	int nonce ;
+} Block ;
+
+
+
+
+
+
+void ecrire_block(char * filename, Block * b);
+
+Block* lire_block(char* filename);
+
+char * block_to_str(Block* b);
