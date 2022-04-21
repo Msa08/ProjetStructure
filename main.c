0023652142a69e47ff2277d42752a81b3dba0261
itminene <itminene@hotmail.com>
2022-04-21 02:39:20 +0200

    fix main.c

--- main.c
+++ main.c
@@ -20,24 +20,25 @@ int main(){
     srand(time(NULL));
 
 /*____________________________TEST PARTIE 1__________________________________________________________*/
-    FILE *f = fopen("modpow.txt", "w");//Pour comparer méthodes d'exponentation modulaire
-    FILE *f2=fopen("modpow2.txt", "w");
-    clock_t time_initial=0;
-    clock_t time_final=0;
-    clock_t time_initial2=0;
-    clock_t time_final2=0;
-    double time;
-    double time2;
+  printf("\n______________________________TEST PARTIE 1___________________________\n");
+    // FILE *f = fopen("modpow.txt", "w");//Pour comparer méthodes d'exponentation modulaire
+    // FILE *f2=fopen("modpow2.txt", "w");
+    // clock_t time_initial=0;
+    // clock_t time_final=0;
+    // clock_t time_initial2=0;
+    // clock_t time_final2=0;
+    // double time;
+    // double time2;
     long a, b;
     int m,p;
 
 
     //Q1.2 Plus grand nombre premier testable en moins de 2 millième de sec?
-    long i;
-    int res=0;
-    int tmp=0;
-    //printf("Plus grand nombre testable avec is_prime_naive() en moins de 0.002s\n");
-    // for(i=50000; time<0.0020;i++){// tant que time< 2 millième de sec itérer
+    // long i;
+    // int res=0;
+    // int tmp=0;
+    // printf("---Plus grand nombre testable avec is_prime_naive() en moins de 0.002s-----\n");
+    // for(i=10000; time<0.0020;i++){// tant que time< 2 millième de sec itérer
     //
     //     time_initial=clock();
     //     tmp=is_prime_naive(i);
@@ -48,19 +49,19 @@ int main(){
     //     }
     //     printf("time : %f, nb : %ld, premier= %d\n",time, i,tmp);
     // }
-    // printf("Plus grand nombre : %ld\n",res);//186763
+    // printf("Plus grand nombre : %d\n",res);//186763
 
 
 
-    // //test de modpow et modpow_naive
-    // printf("\nTest de modpow_naive et modpow\n" );
-    // for(int i=30;i<40;i++){
-    //     m=(int) rand()%i;
-    //     p=(int) rand()%i;
-    //     a=modpow_naive(m,i,p);
-    //     b=modpow(m,i,p);
-    //     printf("%d^%d mod %d -> modpow_naive: %lu, modpow : %lu\n",m,i,p,a,b);
-    // }
+    //Test de modpow et modpow_naive
+    printf("\n---------------Test de modpow_naive et modpow----------------------\n" );
+    for(int i=30;i<40;i++){
+        m=(int) rand()%i;
+        p=(int) rand()%i;
+        a=modpow_naive(m,i,p);
+        b=modpow(m,i,p);
+        printf("%d^%d mod %d ->  modpow_naive: %lu, modpow : %lu\n",m,i,p,a,b);
+    }
 
     // //Q1.5 Comparaison des performances de modpow_naive et modpow
     // //écriture dans les fichiers modpow_naive et modpow
@@ -85,17 +86,17 @@ int main(){
     // fclose(f2);
 
 
-    //generation de cle;
+    //Generation des clés;
+    printf("\n-------------------------Generation de cles------------------------\n");
     //on génère 2 nb premiers de taille comprise entre 3 et 7 bits
-    printf("Generation de 2 nombres premiers-> ");
+    printf("Generation de 2 nombres premiers : ");
      a=random_prime_number(3,7,5000);
      b=random_prime_number(3,7,5000);
     while(a==b){
         b=random_prime_number(3,7,5000);//pour que a et b soient différents
     }
-    printf("a=%lu , b=%lu\n",a,b);// print de a et b
-
-    // generation de clé publique et privé associé à : a et b
+    printf("a=%lu , b=%lu\n",a,b);
+    //generation des valeurs des clés associés à  a et b
     long n;
     long u;
     long s;
@@ -104,45 +105,52 @@ int main(){
         long t= (a-1)*(b-1);
         u=u+t;
     }
-
-    //affichage des cles en haxadécimal
-    printf("Generation de cles -> ");
+    //affichage des valeurs des clés en haxadécimal
     printf("cle publique = (%lx, %lx) \n",s,n);
     printf("cle privee = (%lx, %lx) \n",u,n);
-    //chiffrement
-    printf("Chiffrement -> ");
-    char mess1[50]="hello";
-    int len=strlen(mess1);
-    long *crypted=encrypt(mess1,s,n);
-    printf("Initial message %s \n",mess1);
-    printf("Encoded representation \n");
-    print_long_vector(crypted,len);
-    //dechiffrement
-    printf("Dechiffrement -> ");
-    char* decoded=decrypt(crypted,len,u,n);
-    printf("Decoded: %s\n",decoded);
+    //initialisation des clés
     Key* pKey=malloc(sizeof(Key));
     init_key(pKey, s,n);
     char* str=key_to_str(pKey);
-    printf("%s\n",str);
+    printf("key_to_str -> %s\n",str);
     Key* sKey=str_to_key("(1273,1f15)");
-    printf("%lu,%lu\n",pKey->cle,pKey->n);
-    printf("%lu,%lu\n",sKey->cle,sKey->n);
+    //affichage des cles en décimal
+    printf("cle publique = %lu,%lu\n",pKey->cle,pKey->n);
+    printf("cle privee = %lu,%lu\n",sKey->cle,sKey->n);
+
+
+    //chiffrement
+    printf("\n-----------------Chiffrement/Dechiffrement-------------------------\n");
+    printf("Chiffrement \n");
+    char mess1[50]="hello";
+    int len=strlen(mess1);
+    long *crypted=encrypt(mess1,s,n);//message crypté
+    printf("Initial message = %s \n",mess1);
+    printf("Encoded representation = ");
+    print_long_vector(crypted,len);
+    //dechiffrement
+    printf("Dechiffrement\n");
+    char* decoded=decrypt(crypted,len,u,n);//message decrypté
+    printf("Decoded = %s\n",decoded);
 
 
     /*____________________________TEST PARTIE 2______________________________________________________*/
-    /*//Testing Init Keys
+      printf("\n______________________________TEST PARTIE 2___________________________\n");
+    //Testing Init Keys
+    printf("\n-----------------Generation d'une paire de cle---------------------\n");
     init_pair_keys(pKey, sKey,3,7);
     printf("pKey: %lx , %lx \n", pKey->cle, pKey->n);
     printf("sKey: %lx , %lx \n", sKey->cle, sKey->n);
-//Testing Key Serialization
+    //Testing Key Serialization
     char* chaine = key_to_str(pKey);
     printf("key to str : %s \n", chaine);
     Key* k = str_to_key(chaine);
     free(chaine);
     printf("str to key : %lx , %lx \n", k->cle, k->n);
     free(k);
+
     //Testing signature
+    printf("\n---------------------Generation de signature-----------------------\n");
     //Candidate keys:
     Key* pKeyC = malloc(sizeof(Key));
     Key* sKeyC = malloc(sizeof(Key));
@@ -158,41 +166,43 @@ int main(){
     chaine = signature_to_str(sgn);
     free(sgn->content);
     free(sgn);
-    printf(" signature to str : %s \n", chaine);
+    printf("signature to str : %s \n", chaine);
     sgn = str_to_signature(chaine);
     free(chaine);
     printf("str to signature : ");
     print_long_vector(sgn->content,sgn->size);
-//Testing protected:
+
+    //Testing protected:
+    printf("\n-------------------Generation de declarations signe----------------\n");
     Protected* pr = init_protected(pKey,mess, sgn);
-//Verification:
+    //Verification:
     if (verify(pr)){ printf("Signature valide\n");
-    }
-    else{
+    }else{
     printf("Signature non valide\n");
     }
     chaine = protected_to_str(pr);
-    printf(" protected to str : %s\n", chaine);
+    printf("protected to str : %s\n", chaine);
     pr = str_to_protected(chaine);
     printf("str to protected : %s %s %s\n",key_to_str(pr->pKey),pr->mess,signature_to_str(pr->sgn));
+
     free(pKey);
     free(sKey);
     free(pKeyC);
     free(sKeyC);
     free(sgn);
     free(pr);
-	free(chaine);
+	  free(chaine);
     free(mess);
     free(crypted);
     free(decoded);
     free(str);
-    /*____________________________TEST PARTIE 3______________________________________________________
+    /*____________________________TEST PARTIE 3______________________________________________________*/
     printf("\n______________________________TEST PARTIE 3___________________________\n");
     generate_random_data(4,2);
     printf("\ngenerate keys\n");
     CellKey* voters=read_public_keys("keys.txt");
     print_list_keys(voters);
-    //delete_list_keys(voters);
+    delete_list_keys(voters);
 
     printf("\ngenerate candidates\n");
     CellKey* candidates=read_public_keys("candidates.txt");
@@ -205,10 +215,10 @@ int main(){
     //delete_cell_protected(LCP);
 
 
-    Key* gagnant = compute_winner(decl,candidates,voters, 2,4);
-    printf("gagnant -> %s\n", key_to_str(gagnant));*/
-    return 0;
-
+     Key* gagnant = compute_winner(decl,candidates,voters, 2,4);
+     printf("gagnant -> %s\n", key_to_str(gagnant));
+     return 0;
+}
     //on obtient 215000363
 
     /*Problème :
@@ -217,4 +227,3 @@ int main(){
     -Il y a 3 cle public dans candidates.txt
     -Il y a beaucoup plus de 5 declarations dans declarations.txt
     -compute_winner genere une fois sur deux  hc*/
-}
